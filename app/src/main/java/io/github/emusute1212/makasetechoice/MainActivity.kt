c5e6ea636a0024a9ea51a63a10677ff2da1dc178
YosukeMiyanishi <miyanishi1212@gmail.com>
2020-08-12 00:29:47 +0900

    feat: make add dialog

--- app/src/main/java/io/github/emusute1212/makasetechoice/MainActivity.kt
+++ app/src/main/java/io/github/emusute1212/makasetechoice/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.github.emusute1212.makasetechoice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/io/github/emusute1212/makasetechoice/members/add/AddMemberDialogFragment.kt
@@ -0,0 +1,38 @@
+package io.github.emusute1212.makasetechoice.members.add
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import io.github.emusute1212.makasetechoice.R
+
+class AddMemberDialogFragment : DialogFragment() {
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.dialog_add_member, container, false)
+//    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = dialog ?: Dialog(requireContext())
+        return dialog.apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setContentView(R.layout.dialog_add_member)
+            // DialogFragment をタイトル無しにします
+            window?.setGravity(Gravity.BOTTOM)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            window?.setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+            setCancelable(true)
+        }
+    }
+}
