d59f7b8c4779e59b76c863a665970e8ad9278314
YosukeMiyanishi <miyanishi1212@gmail.com>
2020-08-22 23:58:31 +0900

    feat: BottomNavigationViewを利用した画面遷移の実装

--- app/src/main/java/io/github/emusute1212/makasetechoice/MainActivity.kt
+++ app/src/main/java/io/github/emusute1212/makasetechoice/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dagger.android.support.DaggerAppCompatActivity
 import io.github.emusute1212.makasetechoice.members.MembersViewModel
 import io.github.emusute1212.makasetechoice.splash.SplashScreenFragment
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -20,6 +23,9 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navigation.setupWithNavController(navHostFragment.navController)
         val fragment = SplashScreenFragment().also {
             it.show(supportFragmentManager, SplashScreenFragment.FRAGMENT_TAG)
         }
