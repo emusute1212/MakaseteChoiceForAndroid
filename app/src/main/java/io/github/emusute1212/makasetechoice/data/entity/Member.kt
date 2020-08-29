b6e19809d52fd7b85471f1c1dc54983a7f23cd01
YosukeMiyanishi <miyanishi1212@gmail.com>
2020-08-30 00:19:13 +0900

    chore: MemberをDatabaseのEntity化

--- app/src/main/java/io/github/emusute1212/makasetechoice/data/entity/Member.kt
+++ app/src/main/java/io/github/emusute1212/makasetechoice/data/entity/Member.kt
@@ -1,6 +1,14 @@
 package io.github.emusute1212.makasetechoice.data.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "members")
 data class Member(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     val id: Int,
+    @ColumnInfo(name = "name")
     val name: String
 )
