6738881d03e38c6c83a7c0f38974bb5f20abbe50
emusute1212 <miyanishi1212@gmail.com>
2024-07-19 23:20:32 +0900

    refactor: Remove unused codes

--- app/src/main/java/io/github/emusute1212/makasetechoice/ext/binding/ItemTouchHelperExt.kt
+++
@@ -1,8 +0,0 @@
-package io.github.emusute1212.makasetechoice.ext.binding
-
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
-
-fun ItemTouchHelper.Callback.bindRecyclerView(target: RecyclerView) {
-    ItemTouchHelper(this).attachToRecyclerView(target)
-}
