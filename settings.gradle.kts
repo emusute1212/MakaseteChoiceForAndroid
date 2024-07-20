692e18b2dbbaa41869a8e85cadf7388a4f17476f
emusute1212 <miyanishi1212@gmail.com>
2024-07-20 13:42:34 +0900

    chore: Fix build error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,14 +10,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    // refs: https://github.com/google/play-services-plugins/issues/223
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.google.android.gms.oss-licenses-plugin") {
-                useModule("com.google.android.gms:oss-licenses-plugin:${requested.version}")
-            }
-        }
-    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
