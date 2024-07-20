04026b05f877977dcda2b3c19b901fd81ef158f1
emusute1212 <miyanishi1212@gmail.com>
2024-07-20 22:09:56 +0900

    fix: Change Repository code for testable

--- app/src/main/java/io/github/emusute1212/makasetechoice/data/repository/GroupDataRepository.kt
+++ app/src/main/java/io/github/emusute1212/makasetechoice/data/repository/GroupDataRepository.kt
@@ -1,28 +1,54 @@
 package io.github.emusute1212.makasetechoice.data.repository
 
 import androidx.annotation.WorkerThread
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import io.github.emusute1212.makasetechoice.data.db.MakaseteChoiceDatabase
 import io.github.emusute1212.makasetechoice.data.db.dao.GroupDao
 import io.github.emusute1212.makasetechoice.data.entity.GroupAndMember
 import io.github.emusute1212.makasetechoice.data.entity.Member
 import io.github.emusute1212.makasetechoice.ext.toGroups
 import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
+import javax.inject.Singleton
 
-class GroupDataRepository @Inject constructor(
+interface GroupDataRepository {
+    @WorkerThread
+    fun loadGroups(): Flow<List<GroupAndMember>>
+
+    @WorkerThread
+    fun saveGroups(groups: Map<String, List<Member>>)
+}
+
+private class GroupDataRepositoryImpl(
     private val database: MakaseteChoiceDatabase
-) {
+) : GroupDataRepository {
     private val dao: GroupDao
         get() = database.groupDao()
 
     @WorkerThread
-    fun loadGroups(): Flow<List<GroupAndMember>> {
+    override fun loadGroups(): Flow<List<GroupAndMember>> {
         return dao.loadGroups()
     }
 
     @WorkerThread
-    fun saveGroups(groups: Map<String, List<Member>>) {
+    override fun saveGroups(groups: Map<String, List<Member>>) {
         dao.deleteAll()
         dao.saveGroups(groups.toGroups())
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+class GroupDataRepositoryModule {
+    @Provides
+    @Singleton
+    fun provideGroupDataRepository(
+        database: MakaseteChoiceDatabase
+    ): GroupDataRepository {
+        return GroupDataRepositoryImpl(
+            database = database,
+        )
     }
 }
